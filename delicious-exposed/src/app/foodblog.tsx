f6c1d2ebbad12b0c21d0654be62400add3140d4b
Amy Mummert <armummert4@gmail.com>
2024-11-05 13:14:01 -0800

    added images for readme, updated sanity.cli, removed blog.tsx which is not needed

--- delicious-exposed/src/app/foodblog.tsx
+++ delicious-exposed/src/app/foodblog.tsx
@@ -3,8 +3,9 @@ import styles from './foodblog.module.css';
 import { useEffect, useState } from 'react';
 import { PortableText, PortableTextBlock } from '@portabletext/react';
 import { client } from './sanity-client';
-import Image from 'next/image';
 
+
+// Food Blog Component
 const FoodBlog = () => {
   interface Post {
     _id: string;
@@ -46,9 +47,7 @@ const FoodBlog = () => {
       {posts.map((post) => (
         <div className={styles.post} key={post._id}>
           <h2 className={styles.title}>{post.title}</h2>
-          {post.mainImage && (
-            <Image src={post.mainImage.asset.url} alt={post.title} />
-          )}
+         
           <p>Published on: {new Date(post.publishedAt).toLocaleDateString()}</p>
           <div className={styles.content}>
           <PortableText value={post.body} />
